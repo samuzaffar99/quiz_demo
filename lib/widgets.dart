f58afeff9d0b30b3b2dfdbd06ea0e6404ef017e7
samuzaffar99 <sam.muzaffar99@gmail.com>
2021-11-09 02:44:46 +0500

    add drawer

---
+++ lib/widgets.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class WidgetsDemo extends StatefulWidget {
+  const WidgetsDemo({Key? key}) : super(key: key);
+  final title = "Widgets Demo";
+  @override
+  State<WidgetsDemo> createState() => _WidgetsDemoState();
+}
+
+class _WidgetsDemoState extends State<WidgetsDemo> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
